1ab48d03bbfef0dd6fa4a0b9dea53ad3c1b3f5c0
Claire-ade <110232042+Claire-ade@users.noreply.github.com>
2025-11-14 20:15:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -125,13 +125,11 @@ if acronym:
         st.write(polymer_data[acronym]["nom"])
         st.subheader("Formule chimique")
         st.latex(polymer_data[acronym]["formule"])
-        
+    else:
+        st.error("Acronyme non trouvé. Essayez un autre.")
     if "image_url" in polymer_data[acronym]:
         st.image(
         polymer_data[acronym]["image_url"],
         caption=f"Structure de {polymer_data[acronym]['nom']}",
         use_column_width=True
     )
-    
-    else:
-        st.error("Acronyme non trouvé. Essayez un autre.")
